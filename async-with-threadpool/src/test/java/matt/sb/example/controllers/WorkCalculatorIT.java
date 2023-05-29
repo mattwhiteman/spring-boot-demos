8d4b9765b438cb445be00423e7bf20e45928f8f1
Matt Whiteman <matt.whiteman@gmail.com>
2023-05-29 16:51:15 -0400

    Separate unit and integration tests by profile

--- async-with-threadpool/src/test/java/matt/sb/example/controllers/WorkCalculatorIT.java
+++ async-with-threadpool/src/test/java/matt/sb/example/controllers/WorkCalculatorIT.java
@@ -25,7 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes= Application.class)
 @AutoConfigureMockMvc
-public class WorkCalculatorIntegrationTest {
+public class WorkCalculatorIT {
 
     @Autowired
     private MockMvc mockMvc;
